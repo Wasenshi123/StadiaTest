7bf24112a4d25dcd39ecd64f17b27616ee781acb
Wasenshi <Wasenshi@WASENSHI1>
2016-08-22 03:50:33 +0700

    Changed to UnityWebRequest
    Now we know that the problem might be around the service controller, or
    the server itself. (The body of request got discarded)

--- FabricServer/WebService/Controllers/MessageController.cs
+++ FabricServer/WebService/Controllers/MessageController.cs
@@ -13,11 +13,12 @@ namespace WebService.Controllers
             return "You have said : '" + msg + "' to the server!";
         }
         [HttpPost]
-        public async Task<string> PostMsg([FromBody]string msg)
+        public string PostMsg([FromBody]string msg)
         {
             ServiceEventSource.Current.Message("request detail: \n" + Request);
-            string content = await Request.Content.ReadAsStringAsync();
-            ServiceEventSource.Current.Message("body: " + content);
+
+
+
 
 
             ServiceEventSource.Current.Message("A client has sent a message: " + msg);
--- UnityClient/Assets/Scripts/WebServiceAgent.cs
+++ UnityClient/Assets/Scripts/WebServiceAgent.cs
@@ -1,42 +1,67 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
 
 public class WebServiceAgent : MonoBehaviour {
+	[HideInInspector]
+	public static WebServiceAgent instance;
+
 	[Tooltip("A string to be sent to the server")]
 	public string message = "Hello I sent this from Unity!";
 
 	//Might need to change these to setting from file, for easy config
 	string URL = "localhost";
 	int Port = 25331;
-    WWW con;
+	UnityWebRequest  con;
 	void Start () {
+		if (instance == null) {
+			instance = this;
+			this.Initialize ();
+		} else
+			Destroy (this);
+	}
+
+	void Initialize()
+	{
 		//Initialize the URL for communicating with the server
 		string connectionString = "http://" + URL + ":" + Port + "/api/message";
 
 		//We use simple GET method on Http Protocol, so we utilize the WWW class of mono to handle things
-		con = new WWW(connectionString + "?msg=" + System.Uri.EscapeDataString(message));
+//		con = new WWW(connectionString + "?msg=" + System.Uri.EscapeDataString(message));
 
 		//TODO might need to change to POST method with proper data model if we send more complicated data.
+		Dictionary<string, string> headers = new Dictionary<string, string>();
+		headers.Add("Content-Type", "application/json");
+		headers.Add("Content-Length", message.Length.ToString());
+
+		WWWForm form = new WWWForm();
+		form.AddField("msg", message);
+
+		con = UnityWebRequest.Post(connectionString, form);
 	}
 
+
+	/// <summary>
+	/// Start communicating with the server
+	/// </summary>
     public void Connect()
     {
-        StartCoroutine(_Connect());
+        StartCoroutine(Connect_Proceed());
     }
 
-    IEnumerator _Connect()
+    IEnumerator Connect_Proceed()
     {
 		Debug.Log ("Sending message 'Hello' to the server...");
         
-        yield return con;
+		yield return con.Send();
 
 		if (con.error != null) {
 			Debug.Log ("Connection error! : " + con.error);
 			yield break;
 		}
 
-		string response = con.text;
+		string response = con.downloadHandler.text;
 		Debug.Log ("The response from server: " + response);
     }
 }
